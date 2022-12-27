152d0c80d88fde54a76b2a971191ce6c82d73111
Pat <pwblais@gmail.com>
2022-12-26 22:55:46 -0500

    Hero + Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,18 @@ export default function Hero({ }) {
     return (
         <div className="flex flex-col grow shrink-0 basis-auto justify-center pt-8 pr-0 pb-16 pl-0">
             <div className="flex flex-col">
+                <div className="mb-2 leading-4 text-current">
+                    <span className="text-xl text-yellow">
+                        B.S. Computer Science + Philosophy from <span className="text-red-600">McGill University</span>
+                    </span>
+                </div>
                 <div className="mb-10 leading-4">
-                    <span className="text-8xl text-white block">
+                    <span className="text-6xl md:text-8xl text-white block">
                         Software Engineer
                     </span>
                 </div>
                 <div>
-                    <span className="font-light leading-6 text-4xl text-white">I&apos;m a full stack developer with experience in Crypto, Web3, and Machine Learning.</span>
+                    <span className="font-light leading-6 text-3xl md:text-4xl text-white">I&apos;m a full stack developer with experience in Crypto, Web3, and Machine Learning.</span>
                 </div>
             </div>
         </div>
--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar({ }) {
                 <nav className="flex items-center">
                     <div className="font-normal">
                         <ul className="flex list-none">
-                            <NavItem destination="/about" name="About" />
+                            {/* <NavItem destination="/about" name="About" /> */}
                             <NavItem destination="https://www.github.com/Potrock" name="Github" />
                             <NavItem destination="https://www.twitter.com/Potrock_" name="Twitter" />
                         </ul>
