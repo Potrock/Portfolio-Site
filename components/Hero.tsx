802f9179244b737f645298041456e1dfa3f2c8af
pwblais <pwblais@gmail.com>
2023-01-23 13:40:34 -0500

    Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero({}) {
 	return (
-		<div className="flex flex-col grow shrink-0 basis-auto justify-center pt-8 pr-0 pb-16 pl-0">
+		<div className="flex flex-col grow shrink-0 basis-auto justify-center pt-8 pr-0 pb-12 pl-0">
 			<div className="flex flex-col">
 				<div className="mb-2 leading-4 text-current">
 					<span className="text-sm md:text-xl text-yellow">
